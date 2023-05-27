aac2bf66c84aebc9b5dcf18973875cca7ab406c0
zakee21 <zakee21@gmail.com>
2023-05-26 19:16:11 -0700

    Fix boxes automatically resize to correct width/height

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 //creat 16 x 16 divs which equals 256 individual divs
 //function to create boxes with mouseover eventListeners using a "for" loop
 function divBox (box) {
-    for (i = 0; i < box; i++) {
+    for (i = 0; i < Math.pow(box, 2); i++) {
         const container = document.querySelector('#container');
         const div = document.createElement('div');
         div.classList.add('box');
+        div.style.width = 100 / box + '%'
         div.addEventListener('mouseover', changeBackground);
         container.appendChild(div);
     };
 };
+
 //function to change background color for each box
 function changeBackground() {
     this.style.backgroundColor = 'black';
-    //const test = document.querySelectorAll(".test");
-    //for (i = 0; i < test.length; i++) {
-    //test[i].style.backgroundColor = "black";
-    //console.log('it works??');
-    //};
 };
 
-divBox(15*15);
+//divBox(16);
 
-//100/amount of squares in one row = min width % in css file
-//i.e. 15x15 box: 100/15 = 6.66666, therefore min-width = 6.6666%
+//logic and equation for calculating the width/height of each box:
+//user enters number of boxes per row: 4
+//4 is multiplied by 4 to get total number of boxes: 16
+// box then = 16
+//must divid 100 by the original number entered by user (4),
+    // which can be obtained with finding it's square root
 
 //turn the 256 divs into squares
 //line up the divs into a 16 rows and 16 columns
